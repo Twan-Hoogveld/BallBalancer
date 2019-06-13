523eaaa0c862de36eff1628f21ebafb149adf445
Jaluakbar <31691920+Jaluakbar@users.noreply.github.com>
2019-06-13 21:38:25 +0200

    more trash :)

--- test.py
+++ test.py
@@ -2,18 +2,23 @@ import serial
 import struct
 from time import sleep
 
-COM_PORT = 'COM4' #ENTER COM PORT HERE
+COM_PORT = 'COM2' #ENTER COM PORT HERE
 BAUD_RATE= '9600' #ENTER BAUD RATE HERE' 
 
 arduino = serial.Serial(COM_PORT,BAUD_RATE)
+arduino.write(struct.pack("B",255))
+sleep(3)
+
 
 while True:
-    a = 235
     for i in range(2):
         if i == 0:
-            arduino.write(struct.pack("B",7))
-            sleep(0.7)
+            arduino.write(struct.pack("B",4))
+            sleep(0.80)
+            print(".")
         else:
-            arduino.write(struct.pack("B",15))
-            sleep(0.45)
+            arduino.write(struct.pack("B",12))
+            sleep(0.25)
+            print(",")
+        
         
