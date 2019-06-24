2327a66eff1bcb75d20cab571154a447af9d5b9e
Jaluakbar <31691920+Jaluakbar@users.noreply.github.com>
2019-06-24 11:38:55 +0200

    add ball location gettr and pid

--- ball_det.py
+++ ball_det.py
@@ -17,22 +17,44 @@ ap.add_argument("-v", "--video",
 ap.add_argument("-b", "--buffer", type=int, default=64,
     help="max buffer size")
 args = vars(ap.parse_args())
- 
+
 # define the lower and upper boundaries of the colors in the HSV color space
-lower = {'green':(66, 122, 129),'orange':(0, 60, 87)} 
+lower = {'green':(66, 122, 129),'orange':(0, 60, 87)}
 upper = {'green':(90,255,255), 'orange':(20,255,255)}
 
 # define standard colors for circle around the object
 colors = {'red':(0,0,255), 'green':(0,255,0), 'blue':(255,0,0), 'yellow':(0, 255, 217), 'orange':(0,140,255)}
 
+# PID
+green_setpoint_y = 200
+orange_ball_y = 0;
+
+error_old = 0
+kp = 1
+ki = 1
+kd = 1
+
+def pid_control(ball_height, setpoint, delta_time = 0.02, ):
+    global error_old
+    ie = 0
+    error_new = setpoint - ball_height
+    delta_error = error_new-error_old
+    delta_time = 0.01
+    ie += error_new
+
+    error_old = error_new
+    total_action = kp * error_new + kd * (delta_error/delta_time)+ ki * ie
+    print("total : ", total_action)
+
+
 #pts = deque(maxlen=args["buffer"])
- 
+
 # if a video path was not supplied, grab the reference
 # to the webcam
 if not args.get("video", False):
     camera = cv2.VideoCapture(1)
-    
- 
+
+
 # otherwise, grab a reference to the video file
 else:
     camera = cv2.VideoCapture(args["video"])
@@ -60,13 +82,13 @@ while True:
         mask = cv2.inRange(hsv, lower[key], upper[key])
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-                
+
         # find contours in the mask and initialize the current
         # (x, y) center of the ball
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_SIMPLE)[-2]
         center = None
-        
+
         # only proceed if at least one contour was found
         if len(cnts) > 0:
             # find the largest contour in the mask, then use
@@ -76,23 +98,35 @@ while True:
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        
+
             # only proceed if the radius meets a minimum size. Correct this value for your obect's size
             if radius > 0.5:
                 # draw the circle and centroid on the frame,
                 # then update the list of tracked points
                 cv2.circle(frame, (int(x), int(y)), int(radius), colors[key], 2)
                 cv2.putText(frame,key + " ball", (int(x-radius),int(y-radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.6,colors[key],2)
+                if key == "green":
+                    green_setpoint_y = y
+                elif key == "orange":
+                    orange_ball_y = y;
 
-     
+
+
     # show the frame to our screen
     cv2.imshow("Frame", frame)
-    
+
+    #Print to test, may cause LAG
+    print("Green : ", green_setpoint_y, "  Orange : ", orange_ball_y)
+
+    #PID Call
+    #pid_control(orange_ball_y, green_setpoint_y)
+
+
     key = cv2.waitKey(1) & 0xFF
     # if the 'q' key is pressed, stop the loop
     if key == ord("q"):
         break
- 
+
 # cleanup the camera and close any open windows
 camera.release()
 cv2.destroyAllWindows()
